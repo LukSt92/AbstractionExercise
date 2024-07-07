d052673833865b867671c5d829b71aede80f65fb
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-07-07 11:32:20 +0200

    Add bunch of classes as a sample to understand abstraction concept.

--- AbstractionExercise/Program.cs
+++ AbstractionExercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,39 @@ namespace AbstractionExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(new Venus().MissionInfo());
+            Console.WriteLine(new Mars().MissionInfo());
         }
+            abstract class PlanetMission
+        {
+            protected float fuelPerKm;
+            protected long kmPerHour;
+            protected long kmToPlanet;
+            public string MissionInfo()
+            {
+                long fuel = (long)(kmToPlanet * fuelPerKm);
+                long time = kmToPlanet / kmPerHour;
+                return $"We'll burn {fuel} units of fuel in {time} hours";
+            }
+        }
+        class Mars : PlanetMission
+        {
+            public Mars()
+            {
+                kmToPlanet = 92000000;
+                fuelPerKm = 1.73f;
+                kmPerHour = 37000;
+            }
+        }
+        class Venus : PlanetMission
+        {
+            public Venus()
+            {
+                kmToPlanet = 41000000;
+                fuelPerKm = 2.11f;
+                kmPerHour = 29500;
+            }
+        }
+        
     }
 }
